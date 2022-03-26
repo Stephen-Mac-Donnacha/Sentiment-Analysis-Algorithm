05c18f2f257663956b8a4c8b8b58915470a1ee20
Stephen Mac Donnacha <stephenmcdonagh45@gmail.com>
2022-03-26 12:07:24 +0000

    added extra excel worksheet for frequency data

--- write_data_into_excel.py
+++ write_data_into_excel.py
@@ -18,23 +18,35 @@ def get_profile_data():
 
 
 sentiment_data = get_profile_data()[0]
+frequency_data = get_profile_data()[1]
 
 
 def write_sentiment_data_to_excel():
     # Create worksheet for sentiment data
-    sheet = wb.active
-
-    # One can change the name of the title
-    sheet.title = "Sentiment Data"
+    sentiment_sheet = wb.create_sheet("Sentiment Data")
 
     # print("Printing data to worksheet :", sheet.title)
 
-    sheet['A1'] = "Date"
-    sheet['B1'] = "Sentiment"
+    sentiment_sheet['A1'] = "Date"
+    sentiment_sheet['B1'] = "Sentiment"
 
     for row, (Date, Sentiment) in enumerate(sentiment_data.items(), start=2):
-        sheet[f"A{row}"] = Date
-        sheet[f"B{row}"] = Sentiment
+        sentiment_sheet[f"A{row}"] = Date
+        sentiment_sheet[f"B{row}"] = Sentiment
+
+    wb.save("C:\\Users\\Stephen Mac Donnacha\\PycharmProjects\\Final Year Project\\excel-worksheets\\fyp-data.xlsx")
+
+
+def write_frequency_data_to_excel():
+    # Create worksheet for sentiment data
+    frequency_sheet = wb.create_sheet("Frequency Data")
+
+    frequency_sheet['A1'] = "Date"
+    frequency_sheet['B1'] = "Tweets_posted"
+
+    for row, (Date, Tweets_posted) in enumerate(frequency_data.items(), start=2):
+        frequency_sheet[f"A{row}"] = Date
+        frequency_sheet[f"B{row}"] = Tweets_posted
 
     wb.save("C:\\Users\\Stephen Mac Donnacha\\PycharmProjects\\Final Year Project\\excel-worksheets\\fyp-data.xlsx")
 
@@ -57,6 +69,7 @@ def main():
     # create_workbook()
     # get_profile_data()
     write_sentiment_data_to_excel()
+    write_frequency_data_to_excel()
 
 
 if __name__ == "__main__":
