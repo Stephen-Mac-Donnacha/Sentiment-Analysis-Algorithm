baa84b099d3894ee81b83e77c0ef2a8751a1e3c3
Stephen Mac Donnacha <stephenmcdonagh45@gmail.com>
2022-04-06 12:03:11 +0100

    renamed instances of tweet to post

--- main.py
+++ main.py
@@ -6,13 +6,13 @@ import pandas as pd
 from utility_functions import extract_days, extract_months, sanitise_date, sanitise_str
 
 # Global variables, for ease of use
-profile = pd.read_csv("tweet_collection.csv")
+profile = pd.read_csv("post_collection.csv")
 positive_phrases = pd.read_csv("pos_words.csv")
 negative_phrases = pd.read_csv("neg_words.csv")
 
 
-# Method to capture the sentiment of a tweet
-def analyse_tweet(tweet_str):
+# Method to capture the sentiment of a post
+def analyse_post(post_str):
     pos_sent = 0
     neg_sent = 0
     overall_sent = ""
@@ -28,9 +28,9 @@ def analyse_tweet(tweet_str):
     for i in range(len(neg_phrases)):
         neg_phrases[i] = sanitise_str(neg_phrases[i])
 
-    tweet_str = tweet_str.lower()
+    post_str = post_str.lower()
 
-    for word in tweet_str.split(" "):
+    for word in post_str.split(" "):
         if word in pos_phrases:
             index = pos_phrases.index(word)
             pos_sent += pos_strength[index]
@@ -40,7 +40,7 @@ def analyse_tweet(tweet_str):
         else:
             pass
 
-    # Get the average sentiment of a tweet, by taking the strength of the negative sentiment
+    # Get the average sentiment of a post, by taking the strength of the negative sentiment
     # away from the strength of the positive sentiment
     final_sent = pos_sent - neg_sent
 
@@ -51,7 +51,7 @@ def analyse_tweet(tweet_str):
     else:
         overall_sent = "neutral"
 
-    # Return the overall sentiment for a tweet as well as the score of that tweet
+    # Return the overall sentiment for a post as well as the score of that post
     if overall_sent == "positive":
         return overall_sent, pos_sent
     elif overall_sent == "negative":
@@ -63,28 +63,28 @@ def analyse_tweet(tweet_str):
 # Method to capture the sentiment for an overall day
 def capture_sentiment_days():
     dates = list(profile['date'])
-    tweets = profile['tweet']
-    sent_tweet_array = []
+    posts = profile['post']
+    sent_post_array = []
     sent_strength_array = []
-    days_tweet_sent = {}
+    days_post_sent = {}
 
     for i in range(len(dates)):
         dates[i] = sanitise_date(dates[i])
 
-    # Iterate over the tweets, getting the sentiment for the tweet and the strength of the sentiment
-    for i in range(len(tweets)):
-        res = analyse_tweet(tweets[i])
-        sent_tweet_array.append(res[0])
+    # Iterate over the posts, getting the sentiment for the post and the strength of the sentiment
+    for i in range(len(posts)):
+        res = analyse_post(posts[i])
+        sent_post_array.append(res[0])
         sent_strength_array.append(res[1])
 
     for i in range(len(dates)):
         tmp_key = dates[i]
-        days_tweet_sent[tmp_key] = sent_tweet_array[i]
+        days_post_sent[tmp_key] = sent_post_array[i]
 
-    return days_tweet_sent
+    return days_post_sent
 
 
-# Method to capture the sentiment of a profile, currently a collection of mock tweets
+# Method to capture the sentiment of a profile, currently a collection of mock posts
 def capture_sentiment_profile():
     sent_with_dates = capture_sentiment_days()
     dates = list(profile['date'])
@@ -115,8 +115,8 @@ def capture_sentiment_profile():
 # Method to capture changes in sentiment
 def capture_changes_in_sentiment():
     overall_sentiment = capture_sentiment_profile()
-    days_tweet_sent = capture_sentiment_days()
-    sent_list = list(days_tweet_sent.values())
+    days_post_sent = capture_sentiment_days()
+    sent_list = list(days_post_sent.values())
     date_list = profile['date']
     date_list = list(dict.fromkeys(date_list))
 
@@ -131,8 +131,8 @@ def capture_changes_in_sentiment():
 
 # Method to look for consecutive negative days
 def look_for_consec_neg_days():
-    days_tweet_sent = capture_sentiment_days()
-    list_sent = list(days_tweet_sent.values())
+    days_post_sent = capture_sentiment_days()
+    list_sent = list(days_post_sent.values())
     dates_list = list(profile['date'])
     dates_list = list(dict.fromkeys(dates_list))
     neg_days_counter = 0
@@ -169,20 +169,20 @@ def look_for_consec_neg_days():
 
 # Method to look for sharp changes in sentiment
 def find_sharp_changes():
-    tweets = profile['tweet']
+    posts = profile['post']
     sentiment_name = []
     dates = profile['date']
     difference = 0  # The change between a positive and negative sentiment
 
-    tweet_strengths = []
-    for i in range(len(tweets)):
-        res = analyse_tweet(tweets[i])
-        tweet_strengths.append(res[1])
+    post_strengths = []
+    for i in range(len(posts)):
+        res = analyse_post(posts[i])
+        post_strengths.append(res[1])
         sentiment_name.append(res[0])
 
-    for i in range(len(tweet_strengths) - 1):
+    for i in range(len(post_strengths) - 1):
         if sentiment_name[i] != sentiment_name[i + 1]:
-            difference = tweet_strengths[i] - tweet_strengths[i + 1]
+            difference = post_strengths[i] - post_strengths[i + 1]
             if difference > 0.8:
                 print("There was a sharp change in sentiment on: ", dates[i])
                 print("Sentiment changed from : ", sentiment_name[i], " to: ", sentiment_name[i + 1])
@@ -215,9 +215,9 @@ def changes_in_frequency():
         print("No change in frequency flagged")
 
 
-def get_tweet_amount_by_day():
+def get_post_amount_by_day():
     dates = list(profile['date'])
-    tweets = list(profile['tweet'])
+    posts = list(profile['post'])
     unique_days = list(dict.fromkeys(dates))
     count_per_day = 0
     freq_list = []
@@ -225,7 +225,7 @@ def get_tweet_amount_by_day():
 
     for i in range(len(dates)):
         dates[i] = sanitise_date(dates[i])
-        tweets[i] = sanitise_str(tweets[i])
+        posts[i] = sanitise_str(posts[i])
 
     for i in range(len(unique_days)):
         unique_days[i] = sanitise_date(unique_days[i])
@@ -236,8 +236,8 @@ def get_tweet_amount_by_day():
             freq_list.append(count_per_day)
 
     # Fix / find a better way of doing this! temporary fix
-    tweets_at_final_day = dates.count(dates[-1])
-    freq_list.append(tweets_at_final_day)
+    posts_at_final_day = dates.count(dates[-1])
+    freq_list.append(posts_at_final_day)
 
     for i in range(len(unique_days)):
         tmp_key = unique_days[i]
@@ -246,41 +246,41 @@ def get_tweet_amount_by_day():
     return freq_dict
 
 
-# Method to get the mean number of tweets per day
-def get_mean_tweets():
-    dict = get_tweet_amount_by_day()
+# Method to get the mean number of posts per day
+def get_mean_posts():
+    dict = get_post_amount_by_day()
     mean = statistics.mean(dict.values())
     return math.ceil(mean)
 
 
-# Method to find days with more tweets posted
-def find_days_with_more_tweets():
-    frequency_dict = get_tweet_amount_by_day()
+# Method to find days with more posts posted
+def find_days_with_more_posts():
+    frequency_dict = get_post_amount_by_day()
     dates = list(profile['date'])
-    mean = get_mean_tweets()
-    tweets_lst = list(frequency_dict.values())
+    mean = get_mean_posts()
+    posts_lst = list(frequency_dict.values())
 
-    # Check to see if for any day the user posted more tweets than average
+    # Check to see if for any day the user posted more posts than average
     for i in range(len(frequency_dict)):
-        if tweets_lst[i] > mean:
-            dif = tweets_lst[i] - mean
-            print("On this day: ", dates[i], "There was: ", dif, "more tweets posted than average")
+        if posts_lst[i] > mean:
+            dif = posts_lst[i] - mean
+            print("On this day: ", dates[i], "There was: ", dif, "more posts posted than average")
 
 
-def find_days_with_sharp_increase_in_tweets():
-    freq_dict = get_tweet_amount_by_day()
+def find_days_with_sharp_increase_in_posts():
+    freq_dict = get_post_amount_by_day()
     dates = profile['date']
-    avg = get_mean_tweets()
-    tweet_lst = list(freq_dict.values())
+    avg = get_mean_posts()
+    post_lst = list(freq_dict.values())
 
     for i in range(len(freq_dict)):
-        if tweet_lst[i] >= avg + 3:
+        if post_lst[i] >= avg + 3:
             print("Sharp change in posting found, Happened on this date: ", dates[i])
 
 
 def capture_profile_and_analyse():
     # Perform the entire range of sentiment analysis for the profile
-    # Capture the sentiment for each day a tweet was posted
+    # Capture the sentiment for each day a post was posted
     sentiment_for_profile = capture_sentiment_days()
     print(sentiment_for_profile)
 
@@ -300,17 +300,17 @@ def capture_profile_and_analyse():
     # Capture consecutive days were the sentiment was negative
     look_for_consec_neg_days()
 
-    # Find the amount of tweets per day
-    days_and_tweets = get_tweet_amount_by_day()
-    print("Days and the amount of tweets posted on them: ", days_and_tweets)
+    # Find the amount of posts per day
+    days_and_posts = get_post_amount_by_day()
+    print("Days and the amount of posts posted on them: ", days_and_posts)
 
-    # Find any days with sharp increases in the amount of tweets posted
-    find_days_with_sharp_increase_in_tweets()
+    # Find any days with sharp increases in the amount of posts posted
+    find_days_with_sharp_increase_in_posts()
 
-    # Find any days with more tweets than the average posted
-    find_days_with_more_tweets()
+    # Find any days with more posts than the average posted
+    find_days_with_more_posts()
 
-    return sentiment_for_profile, days_and_tweets
+    return sentiment_for_profile, days_and_posts
 
 
 def main():
